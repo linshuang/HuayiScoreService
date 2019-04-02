be986dd0076539cb743dd73e262dd06208190a22
linshuang <linshuang@baizeai.com>
2019-04-02 23:55:06 +0800

    init

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import Response
 
 app = Flask(__name__)
 
@@ -6,6 +9,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+def credit_score():
+    response = {'score': 0}
+    return jsonify(response)
+
+
+def risk_score():
+    max_count = 100
+    if request.args.get('maxCount') is not None:
+        max_count = int(request.args.get('maxCount'))
+    response = {'score': 0}
+    return jsonify(response)
 
 
 if __name__ == '__main__':
